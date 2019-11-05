062c7ca7c12efb8e31bcebad829e34bab0b86acb
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2019-11-05 18:49:35 +0300

    Add user choice sample

---
+++ examples/AlanSampleUserConfirmation/app/src/main/java/com/almadev/alansampleuserconfirmation/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.almadev.alansampleuserconfirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alan.alansdk.AlanCallback
+import com.alan.alansdk.BasicSdkListener
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        alanButton.initSDK("012cfb9ded14239e64635bba9c3571f42e956eca572e1d8b807a3e2338fdd0dc/stage")
+
+        start_button.setOnClickListener {
+            alanButton.sdk.call("script::choice", "{}") { method, response, error ->
+                if (error.isNullOrEmpty()) {
+                    val json = JSONObject(response)
+                    parseAlanResponse(json)
+                }
+            }
+        }
+    }
+
+    private fun parseAlanResponse(response: JSONObject) {
+        if (response.getString("result") == "left") {
+            radio_left.isChecked = true
+        } else {
+            radio_right.isChecked = true
+        }
+    }
+}
