a40c8bcefc763a47699231ca98b2ff0b627cf2a1
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2020-06-18 17:53:28 +0300

    baic example

---
+++ examples/AlanBasicExample/app/src/main/java/app/alan/basicexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.alan.basicexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alan.alansdk.AlanConfig
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val alanConfig = AlanConfig.builder()
+                .setProjectId("314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod")
+                .build()
+        alan_button.initWithConfig(alanConfig)
+    }
+}
