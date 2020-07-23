2a5c36fb7f047ff745f80fa7f5b9ad2d503293a3
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2020-07-23 18:32:03 +0300

    Highlight logic

--- examples/alan-example-integration-kotlin/app/src/main/java/app/alan/exampleintegrationkotlin/MainActivity.kt
+++ examples/alan-example-integration-kotlin/app/src/main/java/app/alan/exampleintegrationkotlin/MainActivity.kt
@@ -2,11 +2,28 @@ package app.alan.exampleintegrationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.alan.basicexample.R
+import app.alan.exampleintegrationkotlin.highlihts.HighlightAdapter
+import app.alan.exampleintegrationkotlin.highlihts.HighlightItem
+import com.alan.alansdk.AlanCallback
 import com.alan.alansdk.AlanConfig
+import com.alan.alansdk.events.EventCommand
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Create dummy items for highlight list
+    private val dummyHighlightItems =
+        listOf("one", "two", "three", "four", "five")
+        .map {
+            HighlightItem(it, false)
+        }
+
+    //Init adapter with dummy items
+    private val highlightAdapter: HighlightAdapter = HighlightAdapter(dummyHighlightItems)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +32,31 @@ class MainActivity : AppCompatActivity() {
                 .setProjectId("314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod")
                 .build()
         alan_button.initWithConfig(alanConfig)
+
+        rvList.layoutManager = LinearLayoutManager(this)
+        rvList.adapter = highlightAdapter
+
+        //Register Alan callback to handle events from Alan Studio script
+        alan_button.registerCallback(object : AlanCallback() {
+
+            //Register command handler
+            override fun onCommandReceived(eventCommand: EventCommand?) {
+                super.onCommandReceived(eventCommand)
+
+                //Extract command payload from json Object
+                val commandObject = eventCommand?.data?.optJSONObject("data")
+                val command = commandObject?.optString("command")
+                when (command) {
+                    "highlight" -> {
+                        //extract item to highlight and pass it to adapter
+                        val itemToHighlight = commandObject.optString("item")
+                        highlightAdapter.turnHighlight(itemToHighlight)
+                    }
+                    else -> {
+                        Log.d("Main", "Unknown command ${command}")
+                    }
+                }
+            }
+        })
     }
 }
---
+++ examples/alan-example-integration-kotlin/app/src/main/java/app/alan/exampleintegrationkotlin/highlihts/HighlightAdapter.kt
@@ -0,0 +1,65 @@
+package app.alan.exampleintegrationkotlin.highlihts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import app.alan.basicexample.R
+import kotlinx.android.synthetic.main.item_highlight.view.*
+
+/**
+ * Prepare adapter and corresponding classes to show list of items to be highlighted
+ */
+
+/**
+ * Model data class which represents item state
+ */
+data class HighlightItem(
+    val text: String,
+    var isHighlighted: Boolean
+)
+
+class HighlightViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(item: HighlightItem) {
+        itemView.tvText.text = item.text
+
+        // Change card background color if item is highlighted
+        if (item.isHighlighted) {
+            itemView.setBackgroundColor(itemView.resources.getColor(R.color.colorPrimary))
+        } else {
+            itemView.setBackgroundColor(itemView.resources.getColor(android.R.color.white))
+        }
+    }
+}
+
+class HighlightAdapter(
+    private val items: List<HighlightItem>
+) : RecyclerView.Adapter<HighlightViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HighlightViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_highlight, parent, false)
+        return HighlightViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: HighlightViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    fun turnHighlight(item: String) {
+        val idx = items.indexOfFirst {
+            it.text == item
+        }
+
+        //dehighlight all items except picked one
+        items.forEachIndexed { index, highlightItem ->
+          highlightItem.isHighlighted = index == idx
+        }
+        notifyDataSetChanged()
+    }
+}
